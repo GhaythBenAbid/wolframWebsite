140fa327842577b6b15a1193a1eaf6e70c02ebb2
GhaythBenAbid <ghaythbenabid0@gmail.com>
2021-06-06 13:30:56 +0100

    change from yarn to npm

--- Server.js
+++ Server.js
@@ -11,7 +11,7 @@ app.get(/.*/, function(req, res) {
     res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
-const port = 8080
+const port = process.env.PORT || 8080
 app.listen(port)
 
 console.log('app is listening on PORT = 8080')
